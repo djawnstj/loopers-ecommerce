11fdcec0981546eb2db283c9758528b4ec7f6ed3
junseo <djawnstj44@naver.com>
2025-07-16 00:30:07 +0900

    feat: UserRepository 구현

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/user/domain/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.loopers.user.domain
+
+import com.loopers.user.domain.vo.UserId
+
+interface UserRepository {
+    fun existsByUserId(userId: UserId): Boolean
+    fun save(user: User): User
+}
---
+++ apps/commerce-api/src/main/kotlin/com/loopers/user/infrastructure/JpaUserRepository.kt
@@ -0,0 +1,9 @@
+package com.loopers.user.infrastructure
+
+import com.loopers.user.domain.User
+import com.loopers.user.domain.vo.UserId
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface JpaUserRepository : JpaRepository<User, Long> {
+    fun existsByUserId(userId: UserId): Boolean
+}
