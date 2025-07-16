bf5ea88d7ce3cf247845b9a230aea0303c5a2ec6
junseo <djawnstj44@naver.com>
2025-07-17 01:07:03 +0900

    feat: UserRepository userId 로 회원 정보 조회 기능 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/user/domain/UserRepository.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/user/domain/UserRepository.kt
@@ -5,4 +5,5 @@ import com.loopers.user.domain.vo.UserId
 interface UserRepository {
     fun existsByUserId(userId: UserId): Boolean
     fun save(user: User): User
+    fun findByUserId(userId: UserId): User?
 }
