affaa7100c501c330f6cd9cfdebbc58833f36632
junseo <djawnstj44@naver.com>
2025-07-18 01:02:11 +0900

    feat: 포인트 조회 API 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/point/presentation/UserPointV1Controller.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/point/presentation/UserPointV1Controller.kt
@@ -2,9 +2,11 @@ package com.loopers.point.presentation
 
 import com.loopers.auth.presentation.UserId
 import com.loopers.point.application.UserPointFacade
+import com.loopers.point.presentation.dto.BalanceResponse
 import com.loopers.point.presentation.dto.ChargePointRequest
 import com.loopers.point.presentation.dto.ChargePointResponse
 import com.loopers.support.presentation.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -21,5 +23,13 @@ class UserPointV1Controller(
     ): ApiResponse<ChargePointResponse> {
         val result = userPointFacade.increasePoint(userId, request.amount)
         return ApiResponse.success(ChargePointResponse(result.balance))
+    }
+
+    @GetMapping("/api/v1/points")
+    fun getBalance(
+        @UserId userId: String,
+    ): ApiResponse<BalanceResponse> {
+        val result = userPointFacade.getPointBalance(userId)
+        return ApiResponse.success(BalanceResponse(result.balance))
     }
 }
