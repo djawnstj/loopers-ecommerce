a9f480f64aed1abd6a50f25b8223dec465271bbf
junseo <djawnstj44@naver.com>
2025-08-02 15:20:16 +0900

    feature: 상품 좋아요 카운트 증가/감소 도메인 로직

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/product/vo/LikeCount.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/product/vo/LikeCount.kt
@@ -5,6 +5,11 @@ import com.loopers.support.error.ErrorType
 
 @JvmInline
 value class LikeCount(val value: Long) {
+    operator fun inc(): LikeCount = LikeCount(value + 1)
+    operator fun dec(): LikeCount = LikeCount(value - 1)
+
+    fun isZero(): Boolean = this == ZERO
+
     init {
         if (value < 0) {
             throw CoreException(ErrorType.REQUIRED_ZERO_OR_POSITIVE_PRODUCT_LIKE_COUNT)
