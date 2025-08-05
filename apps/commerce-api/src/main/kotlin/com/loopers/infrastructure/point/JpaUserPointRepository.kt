2ecd055c31f84a57b3d035a5b40167280ebd1039
junseo <djawnstj44@naver.com>
2025-08-05 23:12:16 +0900

    feat: 포인트 사용을 위한 비관적 락 포인트 조회

--- apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/point/JpaUserPointRepository.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/point/JpaUserPointRepository.kt
@@ -1,6 +1,7 @@
 package com.loopers.infrastructure.point
 
+import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import com.loopers.domain.point.UserPoint
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface JpaUserPointRepository : JpaRepository<UserPoint, Long>
+interface JpaUserPointRepository : JpaRepository<UserPoint, Long>, KotlinJdslJpqlExecutor
