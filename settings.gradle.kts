a433390d8fefdc852cef234fc9c86a12ddfff28e
hubtwork <hubtwork@gmail.com>
2025-08-31 13:14:42 +0900

    gradle settings sync

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,11 @@ rootProject.name = "loopers-kotlin-spring-template"
 
 include(
     ":apps:commerce-api",
+    ":apps:commerce-streamer",
     ":apps:pg-simulator",
     ":modules:jpa",
     ":modules:redis",
+    ":modules:kafka",
     ":supports:cache",
     ":supports:jackson",
     ":supports:logging",
